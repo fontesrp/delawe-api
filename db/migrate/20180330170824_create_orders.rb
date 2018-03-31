1cd67b5e28a7343840a999cb1104e22bc07f896b
Rodrigo Prado Fontes <fontesr2@gmail.com>
2018-03-30 20:36:25 -0700

    Change types of geoloc columns in orders to float

--- db/migrate/20180330170824_create_orders.rb
+++ db/migrate/20180330170824_create_orders.rb
@@ -2,8 +2,8 @@ class CreateOrders < ActiveRecord::Migration[5.1]
   def change
     create_table :orders do |t|
       t.string :address
-      t.string :latitude
-      t.string :longitude
+      t.float :latitude
+      t.float :longitude
       t.string :aasm_state
       t.float :value
       t.references :store, index: true, foreign_key: { to_table: :users }
